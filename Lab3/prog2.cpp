2eec754081ec78bdfba4fa097516ca496e8873a9
Jędrzej Boruczkowski <jedrek.bor2@gmail.com>
2022-10-27 08:51:33 +0200

    Lab3 - zajecia2

---
+++ Lab3/prog2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int number = 1;
+
+	while(number == 1) {
+
+		cout << number << endl;
+
+	}
+
+	return 0;
+}
---
+++ Lab3/prog3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int number = 1;
+
+	do {
+
+	cout << number << endl;
+
+	}
+	while(number > 0);
+
+}
