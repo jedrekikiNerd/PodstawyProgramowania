920d79da4ca397fcf78799673554f3f33a9e668f
Jędrzej Boruczkowski <jedrek.bor2@gmail.com>
2022-10-27 13:44:55 +0200

    Wszystkie zadania z lab3 z zajęć

--- Lab3/prog1.cpp
+++ Lab3/prog1.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
+using namespace std;
+
 int main(){
 	int numer = 5;
-	
+
 	if(numer % 2 == 0){
-		
+
 		cout << "Liczba parzysta" << endl;
-		
+
 	}
 	else{
-		
+
 		cout << "Liczba nieparzysta" << endl;
-		
+
 	}
-	
+
 	return 0;
 }
---
+++ Lab3/prog1_antyprzyklad.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+//W tym przykładzie kod jest nieczytelny
+
+using namespace std;
+
+int main(){
+	int number = 5;
+	if(number < 5) {
+
+	} else if(number > 3 && number < 9) {
+
+ 	 if(number %2 == 0){
+
+ 	 } else {
+
+  	}
+
+	} else if(number > 12 && number %2 == 0) {
+
+	}
+	return 0;
+}
+
